b61d19e7c16a101449a7d86e6568893bc193591c
AF <gulabsinghrajput008@gmail.com>
2025-02-25 22:08:12 +0530

    Update route.py

--- plugins/route.py
+++ plugins/route.py
@@ -2,7 +2,13 @@ from aiohttp import web
 
 routes = web.RouteTableDef()
 
-
 @routes.get("/", allow_head=True)
 async def root_route_handler(request):
     return web.json_response("Elsa")
+
+from pyrogram import Client, filters
+
+# ग्रुप में मैसेज ब्लॉक करने के लिए
+@Client.on_message(filters.group & filters.text)
+def block_in_groups(client, message):
+    message.reply_text("माफ करें, यह बॉट केवल प्राइवेट चैट (PM) में काम करता है।")
