172b4a996198690c108c43c2d39716fa85bee0d0
AF <gulabsinghrajput008@gmail.com>
2025-02-26 16:04:04 +0530

    Update info.py

--- info.py
+++ info.py
@@ -2,6 +2,9 @@ import re
 import os
 from os import environ
 from Script import script
+from dotenv import load_dotenv
+
+load_dotenv()
 
 id_pattern = re.compile(r'^.\d+$')
 def is_enabled(value, default):
