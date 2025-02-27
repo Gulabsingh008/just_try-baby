211859cc7ea7994b1a34052af8bd3c46fd9a9061
AF <gulabsinghrajput008@gmail.com>
2025-02-27 18:11:20 +0530

    Update ia_filterdb.py

--- database/ia_filterdb.py
+++ database/ia_filterdb.py
@@ -76,7 +76,7 @@ async def get_file_by_name(file_name):
     if file:
         return {
             "file_id": file.file_id,  
-            "file_type": file.file_type if file.file_type else "document"  # ✅ सही फाइल टाइप सेट करें
+            "file_type": file.mime_type.split('/')[0] if file.mime_type else "document"  # ✅ सही फाइल टाइप सेट करें
         }
     return None
 
